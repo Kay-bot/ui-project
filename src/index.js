deee24768bd9cfaf88919dff9931285e1c95e03f
KAY S <KS.KWANRUAN@GMAIL.com>
2020-05-31 12:58:18 +1000

    dropdown

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import Tooltip from "./my-ui/tooltip";
+import Dropdown from "./my-ui/dropdown";
 
+//create a tooltip
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
 tooltip.init();
+
+//create dropdowns
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
+
+export { Dropdown as default };
